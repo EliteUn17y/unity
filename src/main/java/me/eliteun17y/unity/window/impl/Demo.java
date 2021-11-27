52115c1340262be3fa7d987144d8bc9b2644e01c
EliteUn17y <djangsumme@gmail.com>
2021-11-27 10:22:04 +0000

    Replaced "Hello, world!!" with "Hello!"

--- src/main/java/me/eliteun17y/unity/window/impl/Demo.java
+++ src/main/java/me/eliteun17y/unity/window/impl/Demo.java
@@ -7,13 +7,13 @@ import me.eliteun17y.unity.window.Window;
 public class Demo extends Window {
     public Demo() {
         super("Demo", "A hello world window.");
-        width = FontManager.instance.robotoRegular.getStringWidth("Hello, world!!");
-        height = FontManager.instance.robotoRegular.getStringHeight("Hello, world!!");
+        width = FontManager.instance.robotoRegular.getStringWidth("Hello!");
+        height = FontManager.instance.robotoRegular.getStringHeight("Hello!");
     }
 
     @Override
     public void draw(int mouseX, int mouseY, float partialTicks) {
         super.draw(mouseX, mouseY, partialTicks);
-        FontManager.instance.robotoRegular.drawString("Hello, world!!", x, y, UIUtil.getFontColor().getRGB());
+        FontManager.instance.robotoRegular.drawString("Hello!", x, y, UIUtil.getFontColor().getRGB());
     }
 }
