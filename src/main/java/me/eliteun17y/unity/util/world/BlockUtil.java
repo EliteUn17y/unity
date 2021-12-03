8a2dc08d0362f77a2b5648d24f15ca5c9b39482b
EliteUn17y <djangsumme@gmail.com>
2021-12-03 21:26:40 +0000

    attempted surround fix

--- src/main/java/me/eliteun17y/unity/util/world/BlockUtil.java
+++ src/main/java/me/eliteun17y/unity/util/world/BlockUtil.java
@@ -5,9 +5,10 @@ import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 public class BlockUtil {
     public static void placeBlock(BlockPos blockPos, EnumHand hand) {
-        Minecraft.getMinecraft().getConnection().sendPacket(new CPacketPlayerTryUseItemOnBlock(blockPos, EnumFacing.getDirectionFromEntityLiving(blockPos, Minecraft.getMinecraft().player).getOpposite(), hand, 0, 0, 0));
+        Minecraft.getMinecraft().getConnection().sendPacket(new CPacketPlayerTryUseItemOnBlock(blockPos, EnumFacing.getDirectionFromEntityLiving(blockPos, Minecraft.getMinecraft().player).getOpposite(), hand, 90, -45, 0));
     }
 }
